2fa5182b42ba7239fb96527de54749fe4965ecf4
Ryo260 <biza1458@yahoo.co.jp>
2023-08-09 14:40:52 +0900

    天気の修正

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,8 +1,9 @@
 function getWeather(){
   const city = document.getElementById('city').value;
   const apiKey = '6f5b14df8f964d2799543704230908' // 後から記載
-//  const url = 'API_ENDPOINT?city=${city}&appid=${apiKey}`' //適切なエンドポイントを記載
+
   const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`; //適切なエンドポイントを記載
+
   fetch(url)
     .then(response =>{
       if(!response.ok) {
@@ -11,8 +12,13 @@ function getWeather(){
       return response.json();
     })
     .then(data=>{
-      const weather = data.weather[0].description;
-      document.getElementById('result').innerText = `Weather in ${city}: ${weather}`;
+      // 天気の説明を取得
+      const weatherDscription = data.current.condition.text;
+
+      // 結果を表示
+      document.getElementById('result').innerText = `Weather in ${city}: ${weatherDescription}`;
+      // const weather = data.weather[0].description;
+      // document.getElementById('result').innerText = `Weather in ${city}: ${weather}`;
     })
     .catch(error =>{
       document.getElementById('result').innerText = 'Filed to get weather. Error: ' + error.message;
