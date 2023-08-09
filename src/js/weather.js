0d663213f7d04b36c1d0907a71d58841d7ee04a0
Ryo260 <biza1458@yahoo.co.jp>
2023-08-09 14:06:45 +0900

    httpsに変更する

--- src/js/weather.js
+++ src/js/weather.js
@@ -2,7 +2,7 @@ function getWeather(){
   const city = document.getElementById('city').value;
   const apiKey = '6f5b14df8f964d2799543704230908' // 後から記載
 //  const url = 'API_ENDPOINT?city=${city}&appid=${apiKey}`' //適切なエンドポイントを記載
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=ja&units=metric`; //適切なエンドポイントを記載
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=ja&units=metric`; //適切なエンドポイントを記載
   
   fetch(url)
     .then(response =>{
