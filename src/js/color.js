9b1438f86079bb701a5fd805d30995c7db0bb14b
Ryo260 <biza1458@yahoo.co.jp>
2023-08-12 11:29:41 +0900

    グラデーション

--- src/js/color.js
+++ src/js/color.js
@@ -2,8 +2,6 @@ const colorInput = document.getElementById("colorInput");
 const changeColorBtn = document.getElementById("changeColorBtn");
 
 function hexToRgb(hex) {
-  if (!hex) return null;
-
   const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
   hex = hex.replace(shorthandRegex, function (m, r, g, b) {
     return r + r + g + g + b + b;
@@ -17,24 +15,26 @@ function hexToRgb(hex) {
   } : null;
 }
 
+function getRgbFromColorName(colorName) {
+  const tempDiv = document.createElement("div");
+  tempDiv.style.display = "none";
+  tempDiv.style.backgroundColor = colorName;
+  document.body.appendChild(tempDiv);
+  const rgb = getComputedStyle(tempDiv).backgroundColor.match(/\d+/g).map(Number);
+  document.body.removeChild(tempDiv);
+  return rgb;
+}
+
 changeColorBtn.addEventListener("click", function () {
   const colorValue = colorInput.value;
   let rgb;
 
-  // HEX値か色名かを判断
   if (colorValue.startsWith("#")) {
     rgb = hexToRgb(colorValue);
   } else {
-    // 一時的なdivを作成して背景色を設定し、RGB値を取得する方法
-    const tempDiv = document.createElement("div");
-    tempDiv.style.display = "none";
-    tempDiv.style.backgroundColor = colorValue;
-    document.body.appendChild(tempDiv);
-    rgb = getComputedStyle(tempDiv).backgroundColor.match(/\d+/g).map(Number);
-    document.body.removeChild(tempDiv);
+    rgb = getRgbFromColorName(colorValue);
   }
 
   const gradientColor = `rgb(${Math.min(rgb[0] + 50, 255)}, ${Math.min(rgb[1] + 50, 255)}, ${Math.min(rgb[2] + 50, 255)})`;
-
-  document.body.style.background = `linear-gradient(to right, ${colorValue}, ${gradientColor})`;
+  document.body.style.background = `linear-gradient(to right, rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]}), ${gradientColor})`;
 });
