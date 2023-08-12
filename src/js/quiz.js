4f827606fc26f88979e0303639bcc1f3632edf54
Ryo260 <biza1458@yahoo.co.jp>
2023-08-12 16:33:35 +0900

    全体的に変更

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,64 +1,64 @@
 let currentQuestion = 0;
-let score =0;
+let score = 0;
 let questions = [];
 
 // クイズのデータを取得する
-async function fetchQuizData(){ // asyncとは？
-  const response = await fetch("https://opentdb.com/api.php?amount=10&type=multiple");
-  const data = await response.json();
-  console.log(data);
-  return data.result;
+async function fetchQuizData() {
+    const response = await fetch("https://opentdb.com/api.php?amount=10&type=multiple");
+    const data = await response.json();
+    return data.results;
 }
 
 // クイズの表示
-function displayQuiz(questions) {
-  const questionElement = document.getElementById("question");
-  const answersElement = document.getElementById("answers");
+function displayQuiz() {
+    const questionElement = document.getElementById('question');
+    const answersElement = document.getElementById('answers');
 
-  // 問題文の設定
-  questionElement.textContent = questions[currentQuestion].question;  
-  // 回答選択肢の設定
-  const answers = [...questions[currentQuestion].incorrect_answers, questions[currentQuestion].correct_answer];
- 
-  // 選択肢をシャッフル
-  answers.sort(()=>Math.random() - 0.5);
+    // 問題文の設定
+    questionElement.textContent = questions[currentQuestion].question;
 
-  answersElement.innerHTML = "";
-  answers.forEach(answers =>{
-    const li = document.getElementById("li");
-    li.textContent = answer;
-    answersElement.appendChild(li);
+    // 回答選択肢の設定
+    const answers = [...questions[currentQuestion].incorrect_answers, questions[currentQuestion].correct_answer];
+    // 選択肢をシャッフル
+    answers.sort(() => Math.random() - 0.5);
 
-    li.addEventListener('click',() =>{
-      if(answer === questions[currentQuestion].correct_answer){
-        score++;
-      }
-      currentQuestion++;
-      if(currentQuestion < questions.length){
-        displayQuiz(questions);
-      } else{
-        // クイズ終了時のスコア表示
-        questionElement.textContent = "クイズ終了！";
-        document.getElementById("score").textContent = `スコア: ${score} / ${questions.length}`;
-        document.getElementById("next").style.display = 'none';
-      }
+    answersElement.innerHTML = "";
+    answers.forEach(answer => {
+        const li = document.createElement('li');
+        li.textContent = answer;
+        answersElement.appendChild(li);
+
+        li.addEventListener('click', () => {
+            if (answer === questions[currentQuestion].correct_answer) {
+                score++;
+            }
+            currentQuestion++;
+            if (currentQuestion < questions.length) {
+                displayQuiz();
+            } else {
+                // クイズ終了時のスコア表示
+                questionElement.textContent = "クイズ終了！";
+                document.getElementById('score').textContent = `スコア: ${score} / ${questions.length}`;
+                document.getElementById('next').style.display = 'none';
+            }
+        });
     });
-  });
 }
 
 document.getElementById('next').addEventListener('click', () => {
-  currentQuestion++;
-  if (currentQuestion < questions.length) {
-      displayQuiz();
-  } else {
-      // クイズ終了時のスコア表示
-      document.getElementById('question').textContent = "クイズ終了！";
-      document.getElementById('score').textContent = `スコア: ${score} / ${questions.length}`;
-      document.getElementById('next').style.display = 'none';
-  }
+    currentQuestion++;
+    if (currentQuestion < questions.length) {
+        displayQuiz();
+    } else {
+        // クイズ終了時のスコア表示
+        document.getElementById('question').textContent = "クイズ終了！";
+        document.getElementById('score').textContent = `スコア: ${score} / ${questions.length}`;
+        document.getElementById('next').style.display = 'none';
+    }
 });
 
 // 最初にクイズのデータを取得し、表示
-fetchQuizData().then(question =>{
-  displayQuiz(question);
+fetchQuizData().then(fetchedQuestions => {
+    questions = fetchedQuestions;
+    displayQuiz();
 });
