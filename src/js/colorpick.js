7f0ee5f353027c3e7f54452c5cecc3c7d33c7980
Ryo260 <biza1458@yahoo.co.jp>
2023-08-14 22:54:56 +0900

    変更

--- src/js/colorpick.js
+++ src/js/colorpick.js
@@ -1,19 +1,17 @@
 // カラーコードから色を更新する関数
-function updateColor(){
-  let colorCode = document.getElementById("colorInput").value;
-  let preview = document.getElementById("colorPreview");
-
-  preview.style.backgroundColor = colorCode;
+function updateColor() {
+    let colorCode = document.getElementById('colorInput').value;
+    let preview = document.getElementById('colorPreview');
+    preview.style.backgroundColor = colorCode;
 }
 
 // RGB値から色を更新する関数
-function updateRGB(){
-  let r = document.getElementById("rRange").value;
-  let g = document.getElementById("gRange").value;
-  let b = document.getElementById("bRange").value;
-
-  let preview = document.getElementById("colorPreview");
-
-  //RGB値を基にbackgroundColorを更新
-  preview.style.backgroundColor = "rgb(${r}, ${g}, ${b})"
+function updateRGB() {
+    let r = document.getElementById('rRange').value;
+    let g = document.getElementById('gRange').value;
+    let b = document.getElementById('bRange').value;
+    
+    let preview = document.getElementById('colorPreview');
+    // RGB値を元にbackgroundColorを更新
+    preview.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 }
