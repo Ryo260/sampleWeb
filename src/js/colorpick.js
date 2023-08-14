49e6f17810b53194f4a6a80780525de721446b7c
Ryo260 <biza1458@yahoo.co.jp>
2023-08-14 22:46:58 +0900

    カラーピッカーアプリ

---
+++ src/js/colorpick.js
@@ -0,0 +1,19 @@
+// カラーコードから色を更新する関数
+function updateColor(){
+  let colorCode = document.getElementById("colorInput").value;
+  let preview = document.getElementById("colorPreview");
+
+  preview.style.backgroundColor = colorCode;
+}
+
+// RGB値から色を更新する関数
+function updateRGB(){
+  let r = document.getElementById("rRange").value;
+  let g = document.getElementById("gRange").value;
+  let b = document.getElementById("bRange").value;
+
+  let preview = document.getElementById("colorPreview");
+
+  //RGB値を基にbackgroundColorを更新
+  preview.style.backgroundColor = "rgb(${r}, ${g}, ${b})"
+}
