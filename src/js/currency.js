92dda6a4d0d0f7b81c37efde4d060303c56cd42f
Ryo260 <biza1458@yahoo.co.jp>
2023-08-15 13:06:25 +0900

    通貨レートアプリのコミット・プッシュ

---
+++ src/js/currency.js
@@ -0,0 +1,22 @@
+// 外部APIから為替レートを取得する関数
+async function fetchExchangeRate(from,to){
+  //外部APIを呼び出す際のURL
+  const url = `https://api.exchangeratesapi.io/latest?base=${from}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.rates[to];
+}
+
+// 通貨を変換する関数
+
+async function convertCurrency(){
+  const form = document.getElementById("fromCurrency").value;
+  const to = document.getElementById("toCurrency").value;
+  const amount = document.getElementById("amount").value;
+
+  const rate = await fetchExchangeRate(from, to);
+
+  const result = amount * rate;
+
+  document.getElementById("result").innerText = `${amount} ${from} = ${result.toFixed(2)} ${to}`;
+}
